d4146a2e37027f5eeaba32ee4f9bfa1a1c4cd8f7
kiki <qiqi.gu@sjtu.edu.cn>
2024-09-06 05:42:40 +0000

    添加teacher训练的脚本

---
+++ benchmark/scripts/tiny_llama_teacher.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICES=0,1 python src/sparseml/transformers/text_generation_fine_tune.py \
+    --output_dir benchmark/output_dir/tiny_llama_teacher \
+    --recipe benchmark/recipes/tiny_llama_teacher.yaml \
+    --model_name_or_path TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
+    --dataset_name open_platypus \
+    --bf16 \
+    --do_train \
+    --do_eval \
+    --evaluation_strategy epoch \
+    --logging_strategy epoch \
+    --save_strategy epoch \
+    --preprocessing_num_workers 32 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 2 \
+    --max_seq_length 384 \
+    --doc_stride 128 \
+    --seed 42
