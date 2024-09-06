2f2d3a2ebb99ac7f6d5ac690ab0d0ef89c365273
kiki <qiqi.gu@sjtu.edu.cn>
2024-09-06 08:00:57 +0000

    update teacher training script

--- benchmark/scripts/tiny_llama_teacher.sh
+++ benchmark/scripts/tiny_llama_teacher.sh
@@ -1,4 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1 python src/sparseml/transformers/text_generation_fine_tune.py \
+#!/bin/bash
+
+{
+  NM_DISABLE_ANALYTICS=1 CUDA_VISIBLE_DEVICES=0,1 python src/sparseml/transformers/text_generation_fine_tune.py \
     --output_dir benchmark/output_dir/tiny_llama_teacher \
     --recipe benchmark/recipes/tiny_llama_teacher.yaml \
     --model_name_or_path TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
@@ -9,6 +12,8 @@ CUDA_VISIBLE_DEVICES=0,1 python src/sparseml/transformers/text_generation_fine_t
     --evaluation_strategy epoch \
     --logging_strategy epoch \
     --save_strategy epoch \
+    --save_total_limit 1 \
+    --save_only_model \
     --preprocessing_num_workers 32 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
@@ -16,3 +21,4 @@ CUDA_VISIBLE_DEVICES=0,1 python src/sparseml/transformers/text_generation_fine_t
     --max_seq_length 384 \
     --doc_stride 128 \
     --seed 42
+} && mv benchmark/output_dir/tiny_llama_teacher/recipe.yaml benchmark/output_dir/tiny_llama_teacher/recipe.yaml.back
