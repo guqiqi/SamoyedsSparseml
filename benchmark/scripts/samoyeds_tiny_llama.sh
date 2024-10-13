348c53e35c36194c7010230d334f18486e2f43b3
kevinwu <cpwu2017@126.com>
2024-10-13 14:16:41 +0000

    update tiny llama sample

--- benchmark/scripts/samoyeds_tiny_llama.sh
+++ benchmark/scripts/samoyeds_tiny_llama.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 YAML_LISTS=(
-    tiny_llama_12_64
+    # tiny_llama_12_16
+    # tiny_llama_12_16_layer_first
+    # tiny_llama_12_32
+    # tiny_llama_12_32_0rate
+    # tiny_llama_12_32_layer_first
+    # tiny_llama_12_64
 )
 
 for yaml_name in ${YAML_LISTS[@]};do
     echo $yaml_name
+    mkdir -p benchmark/output_dir/gsm8k_${yaml_name}
 
-    # echo "NM_DISABLE_ANALYTICS=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes 1 --standalone --nproc_per_node 2 src/sparseml/transformers/text_generation_fine_tune.py \
-    #     --model_name_or_path TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
+    # NM_DISABLE_ANALYTICS=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes 1 --standalone --nproc_per_node 2 src/sparseml/transformers/text_generation_fine_tune.py \
+    #     --model_name_or_path TinyLlama/TinyLlama_v1.1 \
     #     --dataset_name open_platypus \
-    #     --do_train \
     #     --bf16 \
+    #     --do_train \
     #     --do_eval \
     #     --optim adamw_torch \
     #     --evaluation_strategy epoch \
@@ -19,24 +25,41 @@ for yaml_name in ${YAML_LISTS[@]};do
     #     --save_total_limit 3 \
     #     --per_device_train_batch_size 8 \
     #     --per_device_eval_batch_size 8 \
-    #     --learning_rate 8e-5 \
     #     --max_seq_length 384 \
     #     --doc_stride 128 \
-    #     --preprocessing_num_workers 1 \
+    #     --preprocessing_num_workers 32 \
     #     --seed 42 \
-    #     --num_train_epochs 50 \
+    #     --ddp_find_unused_parameters false \
     #     --recipe benchmark/recipes/${yaml_name}.yaml \
     #     --overwrite_output_dir \
     #     --skip_memory_metrics true \
     #     --report_to none \
-    #     --output_dir benchmark/output_dir/${yaml_name} \  "
+    #     --output_dir benchmark/output_dir/${yaml_name} \
+    #     --distill_teacher benchmark/output_dir/tiny_llama_teacher \
+    #     --gradient_accumulation_steps 4 \
+    #     | tee benchmark/output_dir/${yaml_name}/execution_log.log
+done 
 
-    # CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0,1 python src/sparseml/transformers/text_generation_fine_tune.py \
+YAML_LISTS=(
+    # tiny_llama_12_16_gsm8k
+    tiny_llama_12_32_gsm8k
+    # tiny_llama_12_16_layer_first
+    # tiny_llama_12_32
+    # tiny_llama_12_32_0rate
+    # tiny_llama_12_32_layer_first
+    # tiny_llama_12_64
+)
+
+for yaml_name in ${YAML_LISTS[@]};do
+    echo $yaml_name
+    mkdir -p benchmark/output_dir/gsm8k_${yaml_name}
+
     NM_DISABLE_ANALYTICS=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes 1 --standalone --nproc_per_node 2 src/sparseml/transformers/text_generation_fine_tune.py \
-        --model_name_or_path TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
-        --dataset_name open_platypus \
-        --do_train \
+        --model_name_or_path TinyLlama/TinyLlama_v1.1 \
+        --dataset_name gsm8k \
+        --dataset_config_name main \
         --bf16 \
+        --do_train \
         --do_eval \
         --optim adamw_torch \
         --evaluation_strategy epoch \
@@ -44,17 +67,15 @@ for yaml_name in ${YAML_LISTS[@]};do
         --save_total_limit 3 \
         --per_device_train_batch_size 8 \
         --per_device_eval_batch_size 8 \
-        --learning_rate 8e-5 \
         --max_seq_length 384 \
         --doc_stride 128 \
-        --preprocessing_num_workers 1 \
+        --preprocessing_num_workers 32 \
         --seed 42 \
-        --num_train_epochs 50 \
+        --ddp_find_unused_parameters false \
         --recipe benchmark/recipes/${yaml_name}.yaml \
         --overwrite_output_dir \
         --skip_memory_metrics true \
         --report_to none \
-        --output_dir benchmark/output_dir/${yaml_name}   
+        --output_dir benchmark/output_dir/gsm8k_${yaml_name} \
+        | tee benchmark/output_dir/gsm8k_${yaml_name}/execution_log.log
 done
-
-        # --distill_teacher ./benchmark/output_dir/tiny_llama_teacher \
